73defd53d5fd6c976c9c4a0e79e24357c1bbd375
ParisNicolas <nicolasfranciscoparis@gmail.com>
2024-10-04 19:20:00 -0300

    Rutas q me parece que seran necesarias

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,57 @@ from app import db
 
 main = Blueprint('main', __name__, template_folder='templates')
 
+
+#Home para mostrar los cursos a los que tienes acceso
 @main.route('/')
-def general():
-    return render_template('general.html')
+def home():
+    return render_template('home.html')
+
+#Ruta para crear, editar, eliminar y asignar cursos
+@main.route('/gestion_cursos')
+def gestion_cursos():
+    return render_template('algo.html')
+
+#Crear curso para un determinado profesor (usuario con rol profesor)
+@main.route('/gestion_cursos/crear', methods=['POST'])
+def crear_curso():
+    return render_template('algo.html')
+
+#Editar curso
+@main.route('/gestion_cursos/editar/<curso_id>', methods=['POST'])
+def editar_curso(curso_id):
+    return render_template('algo.html')
+
+#Eliminar curso
+@main.route('/gestion_cursos/eliminar/<curso_id>', methods=['DELETE'])
+def eliminar_curso(curso_id):
+    return render_template('algo.html')
+
+#Asignar curso a un estudiante
+@main.route('/gestion_cursos/asignar/<curso_id>/<estudiante_id>', methods=['POST'])
+def asignar_curso(curso_id, estudiante_id):
+    return render_template('algo.html')
+
+
+
+#-----------SECCION 2---------
+
+#Mostrar el contenido de un curso
+@main.route('/curso/<curso_id>')
+def ver_curso(curso_id):
+    return render_template('algo.html')
+
+#Crear un contenido nuevo (solo profesores)
+@main.route('/curso/<curso_id>/crear', methods=['POST'])
+def crear_contenido(curso_id):
+    return render_template('algo.html')
+
+#Eliminar un contenido (solo profesores)
+@main.route('/curso/<curso_id>/eliminar', methods=['POST'])
+def eliminar_contenido(curso_id):
+    return render_template('algo.html')
+
+#Subir una respuesta a una evaluacion
+@main.route('/evaluacion/<evaluacion_id>', methods=['POST'])
+def subir_respuesta(evaluacion_id):
+    return render_template('algo.html')
